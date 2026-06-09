f20952463d435f9e518348437abb6f1144c6e52e
Manuel <francisco192lopez143@gmail.com>
2026-06-09 16:55:11 -0600

    Agregar Google Analytics y mejoras de registro

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 //import { Inter, Playfair_Display } from 'next/font/google'
+import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/next'
 import { LanguageProvider } from '@/components/providers/language-provider'
 import { AuthProvider } from '@/components/providers/auth-provider'
@@ -61,8 +62,24 @@ export default function RootLayout({
             <Footer />
           </LanguageProvider>
         </AuthProvider>
+
         {process.env.NODE_ENV === 'production' && <Analytics />}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S1B9M8KP1E"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S1B9M8KP1E');
+          `}
+        </Script>
       </body>
     </html>
   )
 }
+
